a720a4dbf96f18bab3332d05ffcc0d50601fd8c0
Camilo Andres Ferrer bustos <caferrerb@MacBook-Pro-de-Camilo.local>
2020-08-17 19:10:38 -0500

    fix message main

--- src/main/java/co/edu/eam/exampleproject/Main.java
+++ src/main/java/co/edu/eam/exampleproject/Main.java
@@ -2,6 +2,6 @@ package co.edu.eam.exampleproject;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo bonito y feliz");
+        System.out.println("Hola mundo cruel y despieadado, bonito y feliz");
     }
 }
